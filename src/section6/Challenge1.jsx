55bfd4593d37af59fcebdffcd7859473aaa8bb57
Aniket <aniket@shauryasoft>
2024-06-05 18:05:05 +0530

    temp

--- src/section6/Challenge1.jsx
+++ src/section6/Challenge1.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles/Sec6challenge.css';
 
 const Challenge1 = () => {
+    const [step, setstep] = useState(1);
+    const [count, setcount] = useState(0);
   return (
     <div className='sec6challenge'>
-      challenge
+      <div className="stepbox sec6cbox">
+        <button className="stepboxsub sec6cbtn" onClick={()=>setstep(step--)}>---</button>
+        Step : {step}
+        <button className="stepboxadd sec6cbtn" onClick={()=>setcount(count--)}>+++</button>
+      </div>
+      <div className="countbox sec6cbox">
+        <button className="countboxsub sec6cbtn">---</button>
+        Count : {count}
+        <button className="countboxadd sec6cbtn">+++</button>
+      </div>
+      <div className="msgbox sec6cbox">
+        Final Value :
+      </div>
     </div>
   )
 }
